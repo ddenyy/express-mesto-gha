361d527d7793455897b9a00ea8b26fb5bfe7e13e
Denis Osipov <ddennios@mail.ru>
2022-04-26 10:12:20 +0300

    fix: исправил обработку лайка по несуществ id карточки

--- routes/cardRoute.js
+++ routes/cardRoute.js
@@ -15,7 +15,7 @@ const {
 router.get('/cards', getCard);
 router.delete('/cards/:cardId', cardIdValidation, removeCardById);
 router.post('/cards', createCardValidation, createCard);
-router.put('/cards/:cardId/likes', likeCard);
+router.put('/cards/:cardId/likes', cardIdValidation, likeCard);
 router.delete('/cards/:cardId/likes', cardIdValidation, dislikeCard);
 
 module.exports = router;
