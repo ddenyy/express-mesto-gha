f187a810c564305a024d1bda363c7ba83a430b16
Denis Osipov <ddennios@mail.ru>
2022-05-11 21:21:50 +0300

    fix: исправил все ошибки после 3 ревью

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser');
@@ -13,6 +14,9 @@ const {
   signInValidation,
 } = require('./middlewares/validations');
 const cors = require('./middlewares/corsHeaders');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
 
 const { PORT = 3000 } = process.env;
 const app = express();
@@ -20,12 +24,24 @@ const app = express();
 mongoose.connect('mongodb://localhost:27017/mestodb');
 
 app.listen(PORT);
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // за 15 минут
+  max: 1000, // можно совершить максимум 1000 запросов с одного IP
+});
+
+app.use(limiter);
+
 // подключаем обработку приходящих данных
 app.use(express.json());
 // подключаем обработку куки, все куки лежат в req.cookie
 app.use(cookieParser());
 // подключаем валидацию запросов чтоб проходил проверку CORS
 app.use(cors);
+// настраиваем заголовки
+app.use(helmet());
+//подключаем логгер запросов
+app.use(requestLogger);
 app.post('/signin', signInValidation, login);
 app.post('/signup', signUpValidation, createUser);
 
@@ -34,6 +50,8 @@ app.use(auth);
 // роуты которым нужна авторизация
 app.use(routersUser);
 app.use(routersCard);
+//подключаем логгер ошибок
+app.use(errorLogger);
 
 app.use('*', (req, res, next) => {
   next(new NotFoundError('Страница не найдена'));
